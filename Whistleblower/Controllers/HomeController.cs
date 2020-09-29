47c764bf946d0d03cf65234520d9106087b71896
Nicklas Söderberg <nicklas.soderberg@hotmail.se>
2020-09-29 13:15:59 +0200

    Added simple create whistle form
    Added simple create whistle form

--- Whistleblower/Controllers/HomeController.cs
+++ Whistleblower/Controllers/HomeController.cs
@@ -26,5 +26,12 @@ namespace Whistleblower.Controllers
 
             return View();
         }
+
+        public ActionResult Whistle()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
---
+++ Whistleblower/Models/WhistleModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Whistleblower.Models
+{
+    public class WhistleModel
+    {
+        [Required(ErrorMessage = "Var vänlig fyll i fältet")]
+        [Display(Name = "Vad handlar din rapport om?")]
+        public string About { get; set; }
+
+        [Required(ErrorMessage = "Var vänlig fyll i fältet")]
+        [Display(Name = "När inträffade detta?")]
+        [StringLength(280, ErrorMessage = "Max 280 tecken")]
+        public string When { get; set; }
+
+        [Required(ErrorMessage = "Var vänlig fyll i fältet")]
+        [Display(Name = "Var inträffade detta?")]
+        public string Where { get; set; }
+
+        [Required(ErrorMessage = "Var vänlig fyll i fältet")]
+        [Display(Name = "Beskriv vad som har hänt.")]
+        public string Description { get; set; }
+    }
+}
