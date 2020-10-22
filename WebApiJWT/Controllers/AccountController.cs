07383c9702baae9ec60b3cb7143a10a2b2d8a2d6
Skander Damoussi <Damoussi.Skander@gmail.com>
2020-10-22 13:57:44 +0200

    added new project for jwt token and logic for token generator

---
+++ WebApiJWT/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiJWT.Controllers
+{
+    public class AccountController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage ValidLogin(string userName, string userPassword)
+        {
+            if (userName == "admin" && userPassword == "admin")
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, TokenManager.GenerateToken(userName));
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadGateway, "Username and password is invalid");
+            }
+        }
+    }
+}
